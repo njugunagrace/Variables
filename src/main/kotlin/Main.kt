45930d3b4353c2fe409b08bb7cb32d33b93f6bac
Grace <njugunagraceewambui@gmail.com>
2023-02-02 11:57:25 +0300

    created a file

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    val fullname = "Grace Wambui"
+    var age = 25
+    var phonenumber = "0740348281"
+    var weightInKg = 55
+    val kenyan: Boolean = true
+    var notkenyan: Boolean = false
+
+    println("fullname")
+
+
+}
